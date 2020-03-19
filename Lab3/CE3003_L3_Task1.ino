69419bf286cc298418dc0718a5acf41091bff087
Hoang Viet <51878941+TheMarvelousWhale@users.noreply.github.com>
2020-03-19 21:48:24 +0800

    Update CE3003_L3_Task1.ino

--- Lab3/CE3003_L3_Task1.ino
+++ Lab3/CE3003_L3_Task1.ino
@@ -1,29 +1,69 @@
 #include <Arduino_FreeRTOS.h>
+#include <semphr.h>
 
+char s1[5];
 const TickType_t xDelay1k  pdMS_TO_TICKS(1000);
-
-static const char *textForTask1 = "Task 1 is here LMAO YEET";
+SemaphoreHandle_t xMutex;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  xTaskCreate(MyTask1, "Task1", 100, (void*) textForTask1, 1 , NULL);
-  // pvTaskCode
-  // Desc
-  // usStackDepth - number of words for task thread
-  // pvParameters   
-  // uxPriority o - configMAX_PRIORITIES - 1
-  // pxCreatedTask - optional and set to NULL
-  vTaskStartScheduler();  
+  Serial.begin(115200);  //set baud rate
+  xMutex = xSemaphoreCreateMutex();
+  if (xMutex ==NULL)
+    Serial.println("Mutex creation failed");
+  xTaskCreate(Task1, "Read from PC",100, NULL ,1,NULL);
+  xTaskCreate(Task2, "Print from PC",100, NULL ,1,NULL);
+  vTaskStartScheduler(); 
 }
 
-static void MyTask1(void *pvParameters) {
-  char *pvString = (char *)pvParameters;
-  while(1)
-  {
-    Serial.println(pvString);
-    vTaskDelay(xDelay1k); //delay 1000ms 
-    }
+
+static void Task1(void *pvParameters) 
+{
+  while(1) {
+  xSemaphoreTake(xMutex,portMAX_DELAY);
+  char t;
+  
+  int i ;
+  i = 0;
+  
+
+  Serial.println("'''''''''''''''''''''Task 1 started'''''''''''''''''''''''''");
+  while (Serial.available() ==0){}
+  while (Serial.available()>0) {
+      vTaskDelay(pdMS_TO_TICKS(100));
+      t = Serial.read(); 
+      //Serial.print("You typed: " );
+      //Serial.print(t);  
+      if (i<4) {   
+        s1[i] = t;
+        i++;
+        //Serial.println();
+      }
+      else{
+        //Serial.println(" - discarded");
+      }
+  }
+  
+
+  Serial.println("'''''''''''''''''''''End of Task 1'''''''''''''''''''''");
+  xSemaphoreGive(xMutex);
+  
+   vTaskDelay(xDelay1k);
+  }
+}
+
+static void Task2(void* pvParameters) {
+  while(1){
+  xSemaphoreTake(xMutex,portMAX_DELAY);
+  Serial.println("'''''''''''''''''''''Start of Task 2'''''''''''''''''''''");
+  s1[4] = '\0'; // terminating the String
+  Serial.print("This is what it comes in: ");
+  Serial.println(s1);
+  Serial.println("'''''''''''''''''''''End of Task 2'''''''''''''''''''''");
+  xSemaphoreGive(xMutex);
+  vTaskDelay(xDelay1k);
+  
+  }
 }
 
 void loop() {
